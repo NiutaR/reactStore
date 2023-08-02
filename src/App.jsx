2a25230d55c18203a9a62b4ff5c75ecf8a5e3c69
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-08-02 14:08:16 +0300

    feat: add a Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import AppRoutes from "./components/Routes/Routes";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
     return (
-        <div>Appp</div>
+        <div className="app">
+            <Header></Header>
+            <div className="container">
+                <Sidebar></Sidebar>
+                <AppRoutes></AppRoutes>
+            </div>
+            <Footer></Footer>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import './styles/index.css';
+import { BrowserRouter } from "react-router-dom";
 //import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
