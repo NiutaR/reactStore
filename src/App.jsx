3422fbe76b4828ba28aa0d6b6a1fa09d8af07d90
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-08-03 11:23:10 +0300

    feat: add sidebar and made the storeReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRoutes from "./components/Routes/Routes";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { useDispatch } from "react-redux";
+import { getCategories } from "./features/categories/categoriesSlice";
 
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch]);
     return (
         <div className="app">
             <Header></Header>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import './styles/index.css';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./features/store";
 //import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
